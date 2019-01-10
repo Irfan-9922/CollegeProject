7b3cef71e518bb81637338913fcb54021ac3cd73
Irfan-9922 <techirfan.9@gmail.com>
2019-01-10 12:59:35 +0530

    test class added

---
+++ CollegeApplication/src/test/java/org/college/app/CollegeAppTest.java
@@ -0,0 +1,14 @@
+package org.college.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CollegeAppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
